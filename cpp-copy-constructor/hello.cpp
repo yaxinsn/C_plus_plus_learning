ce6010863ccb0ba5477fe2aa25a25ff376fa3e7b
yaxinsn <yaxinsn@163.com>
2017-12-25 18:52:24 -0800

    C++ 拷贝构造函数

---
+++ cpp-copy-constructor/hello.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+/*
+ *  拷贝构造函数
+ *
+ * 
+ *
+ *
+ * */
+class Hello{
+
+	char* hello;
+public:
+	int show_hello(char* who)
+	{
+		cout  <<hello << "," << who<<endl;
+		return 0;
+	}
+	// 所以在class体以外定义的方法（函数）都必须在class体内进行声明。
+	void set_hello(char* new_hello);
+	~Hello();
+	Hello(const char* h);//必须在class里进行声明。
+	Hello(const Hello &obj);
+};
+Hello::Hello(const Hello &obj)
+{
+	cout <<"call copy constructor "<<endl;
+	hello = strdup(obj.hello);
+}
+#if 1
+Hello::Hello(const char* h)
+{
+	hello=strdup(h);
+
+}
+#endif
+
+#if 1
+Hello::~Hello()
+{
+	if(hello)
+		free(hello);
+}
+
+void Hello::set_hello(char* new_hello)
+{
+	if(hello)
+		free(hello);
+	hello=strdup(new_hello);
+
+}
+#endif
+
+
+int main()
+{
+	Hello Mh("Hi");
+	Mh.show_hello("abl");
+	Mh.set_hello("welcome ");
+	Mh.show_hello("lisa");
+
+	Hello Mh2=Mh;
+	Mh2.show_hello("sheryl");
+	Mh2.set_hello("Mh2 welcome");
+	Mh.set_hello("Mh1 welcome");
+	cout<< "Mh2 and Mh show hello"<<endl;
+	Mh.show_hello("sheryl");
+	Mh2.show_hello("sheryl");
+}	
