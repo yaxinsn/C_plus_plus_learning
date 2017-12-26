da9a841867deb94e5b1f973b9ccb086108d67f11
yaxinsn <yaxinsn@163.com>
2017-12-25 18:30:00 -0800

    init and add hello.cpp

---
+++ code/hello.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Hello{
+
+	char* hello;
+public:
+	int show_hello(char* who)
+	{
+		cout  <<hello << "," << who<<endl;
+		return 0;
+	}
+	// 所以在class体以外定义的方法（函数）都必须在class体内进行声明。
+	void set_hello(char* new_hello);
+	~Hello();
+	Hello(const char* h);//必须在class里进行声明。
+};
+Hello::Hello(const char* h)
+{
+	hello=strdup(h);
+
+}
+#if 1
+Hello::~Hello()
+{
+	if(hello)
+		free(hello);
+}
+
+void Hello::set_hello(char* new_hello)
+{
+	if(hello)
+		free(hello);
+	hello=strdup(new_hello);
+
+}
+#endif
+
+
+int main()
+{
+	Hello Mh("Hi");
+	Mh.show_hello("abl");
+	Mh.set_hello("welcome ");
+	Mh.show_hello("lisa");
+}	
