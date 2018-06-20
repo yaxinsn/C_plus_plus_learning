f0f1a64ad7adbe1cdf49d87b6dc23131ae927374
yaxinsn <yaxinsn@163.com>
2018-06-21 06:21:14 +0800

    a=b-c的重载

--- operator/test_minus/a.cpp
+++ operator/test_minus/a.cpp
@@ -5,21 +5,21 @@ using namespace std;
 class Line
 {
 public:
-	Line(int len);
+	Line(int l);
 	Line operator-();
+	Line operator-(Line const& t);
 
+	int len;
 	void show(void)
 	{
 		cout <<"len : "<<len<<endl;
 	}
-private:
-	int len;
 
 };
 
-Line::Line(int len)
+Line::Line(int l=99)
 {
-	this->len = len;
+	this->len = l;
 	cout<<"Line constructor: len "<<len<<endl;
 }
 
@@ -31,15 +31,27 @@ Line Line::operator-()
 	return l;
 }
 
+Line Line::operator-(Line const& t)
+{
+	Line l(3);
+	l.len=this->len-t.len;
+	return l;
+}
 
 int main()
 {
 	Line line(40);
-	//cout<<"line : "<<line.len<<endl;
-	line.show();
+	//line.show();
+	cout<<"line : "<<line.len<<endl;
 	Line line2=-line;
 
-	line.show();
-	line2.show();	
+	cout<<"line2 : "<<line2.len<<endl;
+//	line.show();
+//	line2.show();	
+
+	Line t;
+	cout<<"t: "<<t.len<<endl;
+	Line t2=t-line;
+	cout<<"t2: "<<t2.len<<endl;
 	return 0;
 }
