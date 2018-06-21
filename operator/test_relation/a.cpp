0290f36e355a6a86aabbd5590cd9466091f80670
dan.liu <dan@ryatek.com>
2018-06-21 19:43:15 +0800

    增加关系运算符的重载

---
+++ operator/test_relation/a.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+
+class Line
+{
+public:
+	Line(int l);
+	Line operator-();
+	Line operator-(Line const& t);
+	bool operator>(Line const& t);
+
+	int len;
+	void show(void)
+	{
+		cout <<"len : "<<len<<endl;
+	}
+
+};
+
+Line::Line(int l=99)
+{
+	this->len = l;
+	cout<<"Line constructor: len "<<len<<endl;
+}
+
+Line Line::operator-()
+{
+	Line l(2);
+	l.len=-this->len;
+	len=-len; //修改自己的值；这包应该删除 
+	return l;
+}
+
+Line Line::operator-(Line const& t)
+{
+	Line l(3);
+	l.len=this->len-t.len;
+	return l;
+}
+
+bool Line::operator>(Line const& t)
+{
+	return this->len > t.len;
+}
+
+
+int main()
+{
+	Line line(40);
+	//line.show();
+	cout<<"line : "<<line.len<<endl;
+	Line line2=-line;
+
+	cout<<"line2 : "<<line2.len<<endl;
+//	line.show();
+//	line2.show();	
+
+	Line t;
+	cout<<"t: "<<t.len<<endl;
+	Line t2=t-line;
+	cout<<"t2: "<<t2.len<<endl;
+
+	Line big(21);
+	Line small(20);
+	if(big>small)
+	{
+		cout<<"big > small "<<endl;
+	}
+	return 0;
+}
