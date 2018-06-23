c926cc554a284167a962e515f052e8d62e8253b9
dan.liu <dan@ryatek.com>
2018-06-23 18:42:03 +0800

    重载了--的运算符，但是在一些调用--时，再赋值时，没有调用拷贝构造函数，这可能是把operator--的函数默认做成内联了。

---
+++ operator/test_++__/a.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+
+class Line
+{
+public:
+	Line(int l);
+	Line(const Line& T);
+	Line operator--();
+	Line operator--(int);
+
+	int len;
+	void show(void)
+	{
+		cout <<"len : "<<len<<endl;
+	}
+
+};
+
+Line::Line(const Line &T )
+{
+
+	this->len = T.len;
+	cout<<"Line copy constructor: len "<<len<<endl;
+}
+Line::Line(int l=99)
+{
+	this->len = l;
+	cout<<"Line constructor: len "<<len<<endl;
+}
+
+Line Line::operator--()
+{
+	Line t;
+	this->len--;
+	t=*this;
+	return t;
+}
+
+Line Line::operator--(int)
+{
+	Line t;
+	t.len=this->len;
+	this->len--;
+	return t;
+}
+
+int main()
+{
+	Line line(40);
+	Line line2;
+	line2=line--;
+	cout<<"line : "<<line.len<<endl;
+	cout<<"line2 : "<<line2.len<<endl;
+	Line line3=--line;
+	cout<<"line : "<<line.len<<endl;
+	cout<<"line3 : "<<line3.len<<endl;
+
+	return 0;
+}
