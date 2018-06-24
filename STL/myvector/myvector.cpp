3f8a5a0198b16ae096680b7fe170ce38311a2808
yaxinsn <yaxinsn@163.com>
2018-06-25 07:03:42 +0800

    学习了向量的迭代器

--- STL/myvector/myvector.cpp
+++ STL/myvector/myvector.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace ::std;
-
+/*
+ * 自己定义一个向量模板。
+ * iterator
+ *
+ * */
 
 template <class T>
 class myvector{
@@ -9,8 +13,13 @@ public:
 	myvector(int size);
 	T pop_head();
 	int size();
-	
-	
+//向量的迭代器
+typedef	T* iter; 
+	T* begin();
+	T* end();
+	void show();
+
+
 private:
 	T* m_entry;
 	int m_size;
@@ -54,9 +63,51 @@ T myvector<T>::pop_head()
 		throw "vector is empty";
 	}
 }
+template <class T>
+T* myvector<T>::begin()
+{
+	return &m_entry[m_head];
+}
 
+template <class T>
+T* myvector<T>::end()
+{
+	return &m_entry[m_end];
+}
+
+template <class T>
+void myvector<T>::show()
+{
+	//return &m_entry[m_end+1];
+}
+
+/////////////////////////////////////////////////////
+//
+int test_iter()
+{
+
+	myvector<int> t(20);
+	t.push_back(50);
+	t.push_back(68);
+	t.push_back(78);
+
+	myvector<int>::iter myiter;
+
+	myiter=t.begin();
+	
+	for(myiter;myiter != t.end();myiter++)
+	{
+		cout<<"iter "<<*myiter<<endl;
+	}
+
+	return 0;
+}
 int main()
 {
+
+
+	test_iter();
+	return 0;
 	myvector<int> t(20);
 	t.push_back(5);
 	t.push_back(6);
@@ -65,19 +116,20 @@ int main()
 	cout<<"size: "<<t.size()<<endl;
 	int p=t.pop_head();
 	cout<<"pop return :"<<p<<endl;
-try{	
-	cout<<"size: "<<t.size()<<endl;
-	t.pop_head();
-	cout<<"size: "<<t.size()<<endl;
-	t.pop_head();
-	cout<<"size: "<<t.size()<<endl;
-	t.pop_head();
-	cout<<"size: "<<t.size()<<endl;
-}
-catch(char const* msg)
-{
-	cout<<msg<<endl;
-}
+	try{	
+		cout<<"size: "<<t.size()<<endl;
+		t.pop_head();
+		cout<<"size: "<<t.size()<<endl;
+		t.pop_head();
+		cout<<"size: "<<t.size()<<endl;
+		t.pop_head();
+		cout<<"size: "<<t.size()<<endl;
+	}
+	catch(char const* msg)
+	{
+		//t.pop_head可能会溢出，引起异常。
+		cout<<msg<<endl;
+	}
 
 	return 0;
 }
