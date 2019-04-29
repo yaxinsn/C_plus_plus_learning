a739bf083bd480cafed5f02795f96322980efd86
root <root@localhost.localdomain>
2019-04-29 09:25:17 +0800

    printf 的打印方式

---
+++ printf/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+int advance_use()
+{
+	char* p = "ddd yyy %d %%x ddd %s xxx aaa";
+	char* t = "second";
+	printf("%.*s--\n",2000,p);
+	return 0;
+}
+int main()
+{
+   printf ("Characters: %c %c \n", 'a', 65);
+   printf ("Decimals: %d %ld\n", 1977, 650000L);
+   printf ("Preceding with blanks: %10d \n", 1977);
+   printf ("Preceding with zeros: %010d \n", 1977);
+   printf ("Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100);
+   printf ("floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
+   printf ("Width trick: %*d \n", 5, 10);
+   printf ("%s \n", "A string");
+
+   advance_use();
+   return 0;
+}
